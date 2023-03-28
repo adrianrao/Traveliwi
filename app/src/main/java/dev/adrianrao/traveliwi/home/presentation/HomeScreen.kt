0aa2058153988e90c90904a89cd50b57c6e5a320
Adrian Rao <moaresiy@gmail.com>
2023-03-28 12:44:07 -0300

    Se agrega pantalla de Home. Ademas se agrega para que se tome la api key de openai

---
+++ app/src/main/java/dev/adrianrao/traveliwi/home/presentation/HomeScreen.kt
@@ -0,0 +1,11 @@
+package dev.adrianrao.traveliwi.home.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun HomeScreen(
+    viewModel: HomeViewModel = hiltViewModel()
+){
+
+}
